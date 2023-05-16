1078940f82f56ad1c877444cd492c55edb4ce40b
Felix-hans <fhans@wewyse.com>
2023-05-16 10:55:23 +0200

    init

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
