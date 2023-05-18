ecfdfb7bd774e495b4fd0c689f64db62cacf9dec
Felix-hans <fhans@wewyse.com>
2023-05-18 23:10:30 +0200

    first prototype

--- app.py
+++ app.py
@@ -12,5 +12,12 @@ def send():
     print(f"Received data: {data}")
     return "Success", 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/send-editor-content', methods=['POST'])
+def send_editor_content():
+    editor_content = request.form['data']
+    print(f"Received editor content: {editor_content}")
+    # Process the editor content here (e.g., execute Python code)
+    # ...
+    response = "Processed editor content"
+    return response, 200
+
